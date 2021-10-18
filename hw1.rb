106603e0bd296f64ef8dcee9ec31e8ef4025cc2d
Anton <antonkonovalov167@gmail.com>
2021-10-18 18:31:29 +0300

    first hw

---
+++ hw1.rb
@@ -0,0 +1,29 @@
+# frozen string literal: true
+
+class Bucket
+  attr_accessor :items
+  PRICES = { 'potato' => 25, 'tomato' => 35, 'cucumber' => 30, 'cherry' => 20, 'nuts' => 40 }.freeze
+
+  def initialize
+    @items = []
+  end
+
+  def add_item(item)
+    if PRICES.key?(item)
+      @items.push item
+      puts 'item successfully added to the bucket'
+    else
+      puts 'this item can not to be add to the bucket'
+    end
+  end
+
+  def +(joiner)
+    result = @items.push(*joiner.items)
+    joiner.items.clear
+    result
+  end
+
+  def total_sum
+    @items.collect { |value| PRICES[value] }.inject(:+)
+  end
+end
